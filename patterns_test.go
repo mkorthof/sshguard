54cc5e593935e4ea8f29d90a7df471e66a32da42
Kevin Zheng <kevinz5000@gmail.com>
2016-02-13 11:18:28 -0800

    Add assertions to test cases

--- patterns_test.go
+++ patterns_test.go
@@ -1,43 +1,150 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestPatterns(t *testing.T) {
+func checkNoMatch(t *testing.T, p *Parser, s string) {
+	if p.Parse(s) != nil {
+		t.Error("message should not match any attack:", s)
+	}
+}
+
+func checkMatch(t *testing.T, p *Parser, s string) {
+	if p.Parse(s) == nil {
+		t.Error("message should match an attack:", s)
+	}
+}
+
+func checkMatches(t *testing.T, match []string, noMatch []string) {
 	p := NewParser(Patterns)
-	attacks := []string{
+	for _, pattern := range match {
+		checkMatch(t, p, pattern)
+	}
+	for _, pattern := range noMatch {
+		checkNoMatch(t, p, pattern)
+	}
+}
+
+func TestCucipop(t *testing.T) {
+	t.Skip("Not implemented")
+	match := []string{
+		"authentication failure XYZ 6.6.6.0",
+	}
+	nomatch := []string{}
+	checkMatches(t, match, nomatch)
+}
+
+func TestCyrus(t *testing.T) {
+	t.Skip("Not implemented")
+	match := []string{
+		"badlogin: XYZ [6.6.6.0] XYZ SASL XYZ checkpass failed",
+	}
+	nomatch := []string{}
+	checkMatches(t, match, nomatch)
+}
+
+func TestDovecot(t *testing.T) {
+	t.Skip("Not implemented")
+	match := []string{
+		"imap-login: Aborted login (auth failed, 6 attempts): XYZ rip=6.6.6.0, lip=127.0.0.1",
+	}
+	nomatch := []string{}
+	checkMatches(t, match, nomatch)
+}
+
+func TestExim(t *testing.T) {
+	t.Skip("Not implemented")
+	match := []string{
+		"XYZ auth_plaintext authenticator failed for XYZ [6.6.6.0]:14432 I=XYZ : 535 Incorrect authentication data (set_id=test)",
+		"expanded_prompt_plain authenticator failed for (test.host) [6.6.6.0] U=CALLER: 535 Incorrect authentication data (set_id=userx)",
+		"login authenticator failed for vps.o2c.net (User) [6.6.6.0]: 535 Incorrect authentication data (set_id=dog)",
+	}
+	nomatch := []string{}
+	checkMatches(t, match, nomatch)
+}
+
+func TestFreeBSDFtpd(t *testing.T) {
+	t.Skip("Not implemented")
+	match := []string{
+		"FTP LOGIN FAILED FROM 6.6.6.0, XYZ",
+	}
+	nomatch := []string{}
+	checkMatches(t, match, nomatch)
+}
+
+func TestPostfix(t *testing.T) {
+	t.Skip("Not implemented")
+	match := []string{
+		"warning: unknown[6.6.6.0]: SASL LOGIN authentication failed: UGFzc3dvcmQ6",
+	}
+	nomatch := []string{}
+	checkMatches(t, match, nomatch)
+}
+
+func TestProFtpd(t *testing.T) {
+	t.Skip("Not implemented")
+	match := []string{
+		"foo.com (foo.com [6.6.6.0]) XYZ no such user XYZ",
+	}
+	nomatch := []string{}
+	checkMatches(t, match, nomatch)
+}
+
+func TestPureFtpd(t *testing.T) {
+	t.Skip("Not implemented")
+	match := []string{
 		"(XYZ@6.6.6.0) [WARNING] Authentication failed for user XYZ",
+	}
+	nomatch := []string{}
+	checkMatches(t, match, nomatch)
+}
+
+func TestSendmail(t *testing.T) {
+	t.Skip("Not implemented")
+	match := []string{
+		"Relaying denied. IP name lookup failed [6.6.6.0]",
+	}
+	nomatch := []string{}
+	checkMatches(t, match, nomatch)
+}
+
+func TestSSH(t *testing.T) {
+	match := []string{
 		"Bad protocol version identification XYZ from 6.6.6.0",
 		"Connection closed by 6.6.6.0 [preauth]",
 		"Did not receive identification string from 6.6.6.0",
-		"FTP LOGIN FAILED FROM 6.6.6.0, XYZ",
 		"Failed XYZ for XYZ from 6.6.6.0 port 14423 ssh2",
 		"Invalid user  from 6.6.6.0",
 		"Invalid user admin from 6.6.6.0",
 		"Invalid user inexu from 6.6.6.0",
-		"Login failed user=XYZ auth=XYZ host=XYZ [6.6.6.0]",
 		"Received disconnect from 6.6.6.0: 11: Bye Bye [preauth]",
 		"Received disconnect from 6.6.6.0: 11: These aren't the droids we're looking for. [preauth]",
-		"Relaying denied. IP name lookup failed [6.6.6.0]",
 		"User mario from 6.6.6.0 not allowed because XYZ",
-		"XYZ FAIL LOGIN: Client \"6.6.6.0\"",
-		"XYZ auth_plaintext authenticator failed for XYZ [6.6.6.0]:14432 I=XYZ : 535 Incorrect authentication data (set_id=test)",
-		"authentication failure XYZ 6.6.6.0",
-		"badlogin: XYZ [6.6.6.0] XYZ SASL XYZ checkpass failed",
 		"error: PAM: authentication failure for mario from 6.6.6.0",
 		"error: Received disconnect from 6.6.6.0: 14: No supported authentication methods available [preauth]",
 		"error: Received disconnect from 6.6.6.0: 3: com.jcraft.jsch.JSchException: Auth fail [preauth]",
-		"expanded_prompt_plain authenticator failed for (test.host) [6.6.6.0] U=CALLER: 535 Incorrect authentication data (set_id=userx)",
-		"foo.com (foo.com [6.6.6.0]) XYZ no such user XYZ",
-		"imap-login: Aborted login (auth failed, 6 attempts): XYZ rip=6.6.6.0, lip=127.0.0.1",
-		"login authenticator failed for vps.o2c.net (User) [6.6.6.0]: 535 Incorrect authentication data (set_id=dog)",
+	}
+	nomatch := []string{
+		"Received disconnect from 6.6.6.0: 11: disconnected by user",
 		"reverse mapping checking getaddrinfo for XYZ [6.6.6.0] XYZ POSSIBLE BREAK-IN ATTEMPT!",
-		"warning: unknown[6.6.6.0]: SASL LOGIN authentication failed: UGFzc3dvcmQ6",
 	}
+	checkMatches(t, match, nomatch)
+}
+func TestUWImap(t *testing.T) {
+	t.Skip("Not implemented")
+	match := []string{
+		"Login failed user=XYZ auth=XYZ host=XYZ [6.6.6.0]",
+	}
+	nomatch := []string{}
+	checkMatches(t, match, nomatch)
+}
 
-	for _, pattern := range attacks {
-		fmt.Println(p.Parse(pattern))
+func TestVsftpd(t *testing.T) {
+	t.Skip("Not implemented")
+	match := []string{
+		"XYZ FAIL LOGIN: Client \"6.6.6.0\"",
 	}
+	nomatch := []string{}
+	checkMatches(t, match, nomatch)
 }
